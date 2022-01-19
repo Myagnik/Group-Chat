9b0f888f0516f1009a5680fa17a12a93d17bf851
yagnikvaghela2611@gmail.com <yagnikvaghela2611@gmail.com>
2022-01-19 12:11:59 +0530

    close icon and dropdown fixed

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -10,16 +10,16 @@ import "channels"
 // window.$ = jquery
 
   $(document).on('turbo:load', function() {
-    alert("kkejwnfjwe");
+   
     $('#message_body').on('keydown', function(e) {
         if (e.keyCode == 13) {
           $('button').click();
           e.target.value = "";
         };
       });
-    $('.ui.dropdown').dropdown();
-   $('.message .close').on('click', function() {
-    $(this).closest('.message').transition('fade');
-  });
+    $(".close.icon").click(function(){
+        $(this).parent().hide();
+      });
+      $('.ui.dropdown').dropdown();
   })
 import "channels"
