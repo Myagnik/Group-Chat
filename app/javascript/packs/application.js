1bb46505ee5186b9a5d0c40cd1e1348acac61fc2
Myagnik <yagnikvaghela2611@gmail.com>
2025-01-13 11:04:10 +0530

    Cable Config and new page bug fixes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,11 @@
 import "controllers"
-import * as ActiveStorage from "@rails/activestorage"
-import "./channels"
+// import * as ActiveStorage from "@rails/activestorage"
+import "../channels"
 import 'jquery'
 
 $(document).ready(function(){
   console.log("oopsps")
-  $('.ui.dropdown').dropdown();
+  // $('.ui.dropdown').dropdown();
   $('.message .close').on('click', function() {
     $(this).closest('.message').transition('fade');
   });
